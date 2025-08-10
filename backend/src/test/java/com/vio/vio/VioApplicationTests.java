d25371b9a24505c90b2c89d6b605acad12866987
nikhilkumar40 <108416519+nikhilkumar40@users.noreply.github.com>
2025-08-10 19:12:05 +0530

    backend added

---
+++ backend/src/test/java/com/vio/vio/VioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vio.vio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
