794121f02d5c4597abce4b031b88efadee682ca2
nikhilkumar40 <108416519+nikhilkumar40@users.noreply.github.com>
2025-08-16 17:50:50 +0530

    comps tructure added

---
+++ frontend/src/components/HamButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HamButton() {
+  return (
+    <div>HamButton</div>
+  )
+}
+
+export default HamButton
---
+++ frontend/src/components/index.js
@@ -0,0 +1,16 @@
+import HamButton from "./HamButton";
+import Navbar from "./navbar/Navbar";
+import LoginButton from "./LoginButton";
+import Searchbar from "./Searchbar";
+import Logo from "./Logo";
+import Footer from "./footer/Footer";
+
+export
+{
+    HamButton,
+    Navbar,
+    LoginButton,
+    Logo,
+    Searchbar,
+    Footer
+}
